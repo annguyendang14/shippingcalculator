e973fc2b0da9ba3e17cde52141af09f0987b05fa
annguyendang14 <annguyendang14@augustana,edu>
2018-03-12 02:58:32 -0500

    programatically add weight1, lol, no way making it to tomorrow class

--- app/src/main/java/com/example/annguyendang14/shippingcalculator/MyActivity.java
+++ app/src/main/java/com/example/annguyendang14/shippingcalculator/MyActivity.java
@@ -1,36 +1,60 @@
 package com.example.annguyendang14.shippingcalculator;
 
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputType;
 import android.text.TextWatcher;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MyActivity extends AppCompatActivity {
 
-    private ShipItem shipItem;
+    private ArrayList<ShipItem> shipItems;
 
-    private EditText weightET;
-    private TextView baseCostTV;
-    private TextView addedCostTV;
-    private TextView totalCostTV;
-
+    private EditText numpackET;
+    private LinearLayout weightLL;
+    private LinearLayout costLL;
+    private int numpack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_my2);
 
-        shipItem = new ShipItem();
-        weightET = (EditText) findViewById(R.id.editText1);
-        baseCostTV = (TextView) findViewById(R.id.textView4);
-        addedCostTV = (TextView) findViewById(R.id.textView6);
-        totalCostTV = (TextView) findViewById(R.id.textView8);
+        shipItems = new ArrayList<ShipItem>();
+        numpackET = (EditText) findViewById(R.id.editText1);
+        weightLL = (LinearLayout) findViewById(R.id.weigh);
+        costLL = (LinearLayout) findViewById(R.id.cost);
+        numpack = 1;
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        LinearLayout rel1 = new LinearLayout(this);
+        rel1.setOrientation(LinearLayout.HORIZONTAL);
+        rel1.setLayoutParams(params);
+        TextView weight1 = new TextView(this);
+        weight1.setText("1st weight: ");
+        weight1.setTextSize(20);
 
-        weightET.addTextChangedListener(weightTextWatcher);
+        EditText weight1E = new EditText(this);
+        weight1E.setInputType(InputType.TYPE_CLASS_NUMBER);
+        weight1E.setMinWidth(500);
+        
+
+        rel1.addView(weight1);
+        rel1.addView(weight1E);
+        weightLL.addView(rel1);
+
+
+
+
+
     }
 
-    private TextWatcher weightTextWatcher = new TextWatcher() {
+ /*   private TextWatcher weightTextWatcher = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -56,7 +80,7 @@ public class MyActivity extends AppCompatActivity {
         baseCostTV.setText("$" + String.format("%.02f",shipItem.getmBaseCost()));
         addedCostTV.setText("$" + String.format("%.02f",shipItem.getmAddedCost()));
         totalCostTV.setText("$" + String.format("%.02f",shipItem.getmTotalCost()));
-    }
+    }*/
 
 
 }
